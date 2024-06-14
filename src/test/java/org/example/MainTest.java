16d01ba57cf44b87b64d3b853a05637b96c27c85
uscmoore <cam68@email.sc.edu>
2024-06-14 14:13:30 -0400

    initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testAddNumbers() {
+        int a = (int) (Math.random() * 10);
+        int b = (int) (Math.random() * 10);
+        int result = Main.addIntegers(a, b);
+        System.out.println("Generated numbers: " + a + ", " + b + "\n" +
+                "Should be: " + (a + b) + "\n" +
+                "Got: " + result);
+        assertEquals(a + b, Main.addIntegers(a, b));
+    }
+}
